d5ca9655fb1a8e091fba15c9ce7a5cebcd140411
RAKIBUL424 <rakibul.r1991@gmail.com>
2024-07-11 02:33:38 +0600

    second commit

--- first_program.py
+++ first_program.py
@@ -1,1 +1,9 @@
 print('Hello World')
+# addition function
+def first_function():
+    a=int(input("Enter Your First Number :"))
+    b=int(input("Enter Your Second Number :"))
+    sum= a+b
+    return sum
+result=first_function()
+print(f"result is {result}")
